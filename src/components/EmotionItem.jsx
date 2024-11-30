23a17fd6ddaeaa249c45861471585ab7852318bb
secgyu <abc2232002@gmail.com>
2024-12-01 02:54:18 +0900

    first commit

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./EmotionItem.css";
+import { getEmotionImage } from "../util/getEmotionImage";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
+  return (
+    <div onClick={onClick} className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
---
+++ src/pages/New.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from "react";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
+
+const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  usePageTitle("새 일기 쓰기");
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
+
+  return (
+    <div>
+      <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />} />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
+};
+
+export default New;
